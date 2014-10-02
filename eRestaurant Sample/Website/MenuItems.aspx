2b91ead94078299515b71cc7ef1b0c49758890aa
Dan Gilleland <dgilleland@nait.ca>
2014-10-02 09:34:01 -0600

    Added BLL method and revised MenuItems.aspx

--- eRestaurant Sample/Website/MenuItems.aspx
+++ eRestaurant Sample/Website/MenuItems.aspx
@@ -1,6 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="MenuItems.aspx.cs" Inherits="MenuItems" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="Server">
+<%-- Original menu repeater that showed Item entities
     <asp:Repeater ID="MenuItemRepeater" runat="server" DataSourceID="MenuItemDataSource">
         <ItemTemplate>
             <%# ((decimal)Eval("CurrentPrice")).ToString("C") %> 
@@ -10,7 +11,27 @@
         <SeparatorTemplate>
             <hr />
         </SeparatorTemplate>
+    </asp:Repeater>--%>
+    <asp:Repeater ID="MenuItemRepeater" runat="server" DataSourceID="MenuItemDataSource">
+        <ItemTemplate>
+            <img src="http://placehold.it/150x100/" alt="" /> <%# Eval("Description") %>
+            <asp:Repeater ID="ItemDetailRepeater" runat="server" DataSource='<%# Eval("MenuItems") %>'>
+                <ItemTemplate>
+                    <div>
+                        <%# Eval("Description") %> &mdash; 
+                        <%# Eval("Calories") %> &mdash;
+                        <%# ((decimal)Eval("Price")).ToString("C") %>
+                        <br />
+                        <%# Eval("Comment") %>
+                    </div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </ItemTemplate>
+        <SeparatorTemplate>
+            <hr />
+        </SeparatorTemplate>
     </asp:Repeater>
-    <asp:ObjectDataSource runat="server" ID="MenuItemDataSource" OldValuesParameterFormatString="original_{0}" SelectMethod="ListMenuItems" TypeName="eRestaurant.BLL.MenuController"></asp:ObjectDataSource>
+
+    <asp:ObjectDataSource runat="server" ID="MenuItemDataSource" OldValuesParameterFormatString="original_{0}" SelectMethod="ListCategorizedMenuItems" TypeName="eRestaurant.BLL.MenuController"></asp:ObjectDataSource>
 </asp:Content>
 
--- eRestaurant Sample/eRestaurant/BLL/MenuController.cs
+++ eRestaurant Sample/eRestaurant/BLL/MenuController.cs
@@ -6,7 +6,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity; // Needed for the Lambda version of the Include() method
+using System.Data.Entity;
+using eRestaurant.Entities.DTOs; // Needed for the Lambda version of the Include() method
 
 namespace eRestaurant.BLL
 {
@@ -22,6 +23,31 @@ namespace eRestaurant.BLL
                 // Get the Item data and include the Category data for each item
                 // The .Include() method on the DbSet<T> class performs "eager loading" of data.
                 return context.Items.Include(it => it.Category).ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Category> ListCategorizedMenuItems()
+        {
+            using (var context = new RestaurantContext())
+            {
+                var data = from cat in context.MenuCategories
+                           orderby cat.Description
+                           select new Category()
+                           {
+                               Description = cat.Description,
+                               MenuItems = from item in cat.MenuItems
+                                           where item.Active
+                                           orderby item.Description
+                                           select new MenuItem()
+                                           {
+                                               Description = item.Description,
+                                               Price = item.CurrentPrice,
+                                               Calories = item.Calories,
+                                               Comment = item.Comment
+                                           }
+                           };
+                return data.ToList();
             }
         }
     }
